524e253a2a29f82fb8462257b6b8cd217f915ffd
Kenneth Cubilo <ccnhs_cubilo0098@calbayog.ph.education>
2025-08-18 03:35:45 +0800

    Fixed footer

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -1,6 +1,8 @@
 <footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>All rights reserved</b>
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2025-2026 <a href="https://www.instagram.com/kenphotographs_/" target="_blank">Kenneth Cubilo</a></strong>
     </div>
-    <strong>Copyright &copy; 2025-2026 <a href="https://www.instagram.com/kenphotographs_/">Kenneth Cubilo</a></strong>
 </footer>
